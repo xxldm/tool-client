51e5413191b830b32d43c21bad2af4d08a12ee48
乡下来的喵 <38677850+xxldm@users.noreply.github.com>
2022-09-03 16:49:15 +0800

    chore: 因为每月的天数不同,导致test结果有误

--- test/time.test.ts
+++ test/time.test.ts
@@ -5,18 +5,13 @@ export default {};
 it("time", async () => {
   await setLocale("zh-cn");
   expect(formatDiff(0)).toMatchInlineSnapshot("\"0 秒\"");
-  expect(formatDiff(1)).toMatchInlineSnapshot("\"1 秒\"");
   expect(formatDiff(60)).toMatchInlineSnapshot("\"1 分\"");
-  expect(formatDiff(61)).toMatchInlineSnapshot("\"1 分, 1 秒\"");
   expect(formatDiff(60 * 60)).toMatchInlineSnapshot("\"1 小时\"");
-  expect(formatDiff(60 * 60 + 1)).toMatchInlineSnapshot("\"1 小时, 1 秒\"");
   expect(formatDiff(24 * 60 * 60)).toMatchInlineSnapshot("\"1 天\"");
-  expect(formatDiff(24 * 60 * 60 + 1)).toMatchInlineSnapshot("\"1 天, 1 秒\"");
-  expect(formatDiff(60 * 24 * 60 * 60)).toMatchInlineSnapshot("\"1 月, 29 天\"");
-  expect(formatDiff(460 * 24 * 60 * 60)).toMatchInlineSnapshot("\"1 年, 3 月\"");
-  expect(formatDiff(460 * 24 * 60 * 60 + 61, 3)).toMatchInlineSnapshot("\"1 年, 3 月, 3 天\"");
-  expect(formatDiff(460 * 24 * 60 * 60 + 61, 4)).toMatchInlineSnapshot("\"1 年, 3 月, 3 天, 1 分\"");
-  expect(formatDiff(460 * 24 * 60 * 60 + 61, 5)).toMatchInlineSnapshot("\"1 年, 3 月, 3 天, 1 分, 1 秒\"");
+  expect(formatDiff(25 * 60 * 61 + 1, 2)).toMatchInlineSnapshot("\"1 天, 1 小时\"");
+  expect(formatDiff(25 * 60 * 61 + 1, 3)).toMatchInlineSnapshot("\"1 天, 1 小时, 25 分\"");
+  expect(formatDiff(25 * 60 * 61 + 1, 4)).toMatchInlineSnapshot("\"1 天, 1 小时, 25 分, 1 秒\"");
+  expect(formatDiff(25 * 60 * 61 + 1, 5)).toMatchInlineSnapshot("\"1 天, 1 小时, 25 分, 1 秒\"");
   expect(formatDiff(
     new Date("2022-08-29 20:30:00"),
     new Date("2022-08-29 20:30:00")))
